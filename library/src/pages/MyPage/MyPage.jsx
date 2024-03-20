59a2f9b03948e396effe33f132681c5914f7103c
Byeonjichan <jaekyeon14@naver.com>
2024-03-20 17:10:26 +0900

    :03.20

--- library/src/pages/MyPage/MyPage.jsx
+++ library/src/pages/MyPage/MyPage.jsx
@@ -1,13 +1,38 @@
 /** @jsxImportSource @emotion/react */
-import { useQueryClient } from "react-query";
+import { useMutation, useQueryClient } from "react-query";
 import * as s from "./style";
-
+import { sendAuthMailRequest } from "../../apis/api/sendAuthMail";
+import FullsizeLoader from "../../components/FullSizeLoader/FullSizeLoader"
+import { GoCheckCircle } from "react-icons/go";
 
 function MyPage(props) {
     const queryClient = useQueryClient();
     const pricipalData = queryClient.getQueryData("principalQuery")
 
+    const sendAuthMailMutation = useMutation({
+        mutationKey: "sendAuthMailMutation",
+        mutationFn: sendAuthMailRequest,
+        retry: 0,
+        onSuccess: (response) => {
+            if(response.data) {
+                alert("메일 전송을 완료하였습니다.")
+            } else {
+                alert("메일 전송이 실패하였습니다.")
+            }
+        }
+
+    });
+
+    const handleSendAuthMailClick = () => {
+        sendAuthMailMutation.mutate();
+    }
+
     return (
+        <>
+        {
+            sendAuthMailMutation.isLoading 
+            ? <FullsizeLoader />
+            :
         <div css={s.layout}>
             <div css={s.header}>
                 <div css={s.imgBox}>
@@ -23,14 +48,14 @@ function MyPage(props) {
                         {
                             pricipalData.data.authorities.filter(auth => auth.authority === "ROLE_USER").length === 0
                             ?
-                            <button css={s.infoButton}>인증하기</button>
+                            <button css={s.infoButton} onClick={handleSendAuthMailClick}>인증하기</button>
                             :
-                            <>체크</>
+                            <div css={s.emailCheck}><GoCheckCircle /></div>
                         }
                     </div>
-                    <div>
-                        <button>정보 수정</button>
-                        <button>비밀번호 수정</button>
+                    <div css={s.infoButtons}>
+                        <button css={s.infoButton}>정보 수정</button>
+                        <button css={s.infoButton}>비밀번호 수정</button>
                     </div>                   
                 </div>
             </div>
@@ -38,6 +63,8 @@ function MyPage(props) {
 
             </div>
         </div>
+        }
+        </>
     );
 }
 
